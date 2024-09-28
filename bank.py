8fe587bf56d28e0390f456ce47173618de50b200
Suman Kumar <90170380+sumankr001@users.noreply.github.com>
2024-09-28 18:16:20 +0530

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,13 @@
+'''Run your program with python bank.py. Type Hello and press Enter. Your program should output:
+$0 
+Run your program with python bank.py. Type Hello, Newman and press Enter. Your program should output:
+$0
+Run your program with python bank.py. Type How you doing? and press Enter. Your program should output
+$20
+Run your program with python bank.py. Type What's happening? and press Enter. Your program should output
+$100
+You can execute the below to check your code using check50, a program that CS50 will use to test your code when you submit. But be sure to test it yourself as well!
+'''
+
+
+
---
+++ interpreter.py
@@ -0,0 +1,72 @@
+
+# s = input("enter any word:  ")
+# a = lambda s : len(s)
+
+# # print(a(s))
+
+# from itertools import groupby
+
+# objects = ["suman", "para", "puja", "ram", "krishna", "haldiram"]
+
+# group = groupby(sorted(objects), key = lambda x : x[0])
+# print(list(group))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# new_object = []
+
+# for i in objects:
+#     new_object.append(i[0])
+
+# word = ''.join(new_object)
+# print(word)
+# new_objects = sorted(objects, key= lambda x : len(x))
+
+# print(new_objects)
+
+
+# y = [3.4,6,8,9,76,74]
+# z = []
+
+# def filters():
+#     for i in y:
+#         if i %2 ==0:
+#             z.append(i)
+
+#     return z
+
+# print(filters())
+
+
+
+# data = {'a' : 48, 'b' :34 , 'c' : 3334, 'x' : 45}
+
+# new_data = max(data, key= lambda x : data[x])
+
+# print(new_data)
+
+# new_data  = []
+
+# for i in data:
+#     new_data.append(data[i])
+
+# print(new_data)
+
+
+# x = 1
+# x = int(x)
+# while (x < 7):
+#     if x == 4:
+#         break
+#     print(x)
